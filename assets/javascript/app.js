48195f76af8546c6ad30f7904a60335747a53376
Robert Bethencourt <robert.bethencourt@gmail.com>
2016-05-01 16:20:45 -0400

    question timer added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -101,6 +101,7 @@ $(document).ready(function(){
 		question_timer: 30,
 		question_timer_display: $('#question-timer-display'), // p
 		q_and_correct_incorrect_display: $('#q-and-correct-incorrect-display'), // h2
+		answers_li: $('.answers-li'),
 
 
 		fadeInGameScreen: function () {
@@ -109,7 +110,7 @@ $(document).ready(function(){
 			$(this.game_screen).fadeIn(500);
 		},
 
-		startGame: function (e) {
+		startGame: function () {
 
 			//fade out the start game screen
 			$(this.start_game_screen).fadeOut(500);
@@ -119,6 +120,43 @@ $(document).ready(function(){
 				// not sure why, but I couldn't use the this keyword here and had to call the triviaGame object
 				triviaGame.fadeInGameScreen();
 			}, 1000);
+
+			// Start the question countdown once the game screen appears
+			setTimeout(function () {
+				triviaGame.questionTime();
+			}, 1000)
+		},
+
+		timer: null,
+
+		questionTime: function () {
+
+			// I needed to create an anonymous function to not return undefined
+			this.timer = setInterval(function () {
+				triviaGame.countQuestionTime();
+			}, 1000);
+		},
+
+		countQuestionTime: function () {
+
+			// reduce the amount of time by one
+			this.question_timer--;
+			// update the screen with the current available time
+			this.question_timer_display.html(this.question_timer);
+
+			if (this.question_timer === 0) {
+				clearInterval(this.timer);
+				this.guess();
+			}
+		},
+
+		displayQandA: function () {
+			
+			q_and_correct_incorrect_display.html(qs_and_as[current_question].question);
+		},
+
+		guess: function () {
+			console.log("yo");
 		}
 	}
 
